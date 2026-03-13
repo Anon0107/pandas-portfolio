a1727cf2290fe417cd5b5bb2dfa00ab27c7b8aa2
Anon0107 <liowks0107@gmail.com>
2026-03-13 18:36:59 +0800

    Update data_cleaner1.py

--- data_cleaner1.py
+++ data_cleaner1.py
@@ -5,20 +5,29 @@ try:
     df = pd.read_excel('sample_laptop_sales.xlsx')
 except FileNotFoundError:
     print("File not found. Make sure it's uploaded.")
-    # Stop here if error
-# Simple change: add column 'Profit per Sale' and 'Total Profit'
+    raise
+except Exception as e:
+    print(f'File not found: {e}')
+    raise  # Stop running when error occured
 
-df['Profit per Sale'] = df.Sales - df.Cost
-df['Total Profit'] = df['Profit per Sale'] * df.Amount
+try:
+    # Add column Profit per Sale and Total Profit
+    df['Profit per Sale'] = df.Sales - df.Cost
+    df['Total Profit'] = df['Profit per Sale'] * df.Amount
+    
+    # Show total profit by region
+    print(df.groupby('Region')['Total Profit'].sum())
+    
+    # Delete products with Profit < 0
+    df = (df.drop(index = df.loc[df['Profit per Sale'] <0].index).reset_index(drop = True))
+except KeyError as e:
+    print(f'Column not found: {e}')
+    raise
+except Exception as e:
+    print(f'Other error: {e}')
+    raise # Stop running when error occured
 
-# Show total profit by region
-print(df.groupby('Region')['Total Profit'].sum())
-
-# Delete products with Profit < 0
-df = (df.drop(index = df.loc[df['Profit per Sale'] <0].index).reset_index(drop = True))
-
-# Save new file
-df.to_excel('Cleaned_sample_laptop_sales.xlsx', index=False)
+df.to_excel('Cleaned_sample_laptop_sales.xlsx', index=False) # Save cleaned file
 
 print("Done! Check Cleaned_sample_laptop_sales.xlsx")
 
