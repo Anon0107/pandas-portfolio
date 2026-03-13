553db584ef0b832aa4490bed7d3bf94577cdd038
Anon0107 <liowks0107@gmail.com>
2026-03-13 18:46:22 +0800

    Update and rename data_cleaner4.py to data_cleaner3.py

--- data_cleaner3.py
+++ data_cleaner3.py
@@ -3,15 +3,16 @@ import pandas as pd
 try:
   df = pd.read_excel('grades_sample.xlsx')
 except FileNotFoundError:
-  print(f"File not found. Make sure it's uploaded.")#Stop here if file not found
+  print(f"File not found. Make sure it's uploaded.")
   raise
 except Exception as e:
   print(f'Error loading file: {e}')
+  raise #Stop here if error
 
 #Data cleaning
 try:
-  df = df.drop_duplicates()
-  df['Math Score'] = df['Math Score'].fillna(df['Math Score'].mean().round(0)).astype(int)
+  df = df.drop_duplicates() # Remove duplicates
+  df['Math Score'] = df['Math Score'].fillna(df['Math Score'].mean().round(0)).astype(int) # Handle Missing Values
   df['Physics Score'] = df['Physics Score'].fillna(df['Physics Score'].mean().round(0)).astype(int)
 except KeyError as e:
   print(f'Column not found: {e}')
@@ -19,6 +20,6 @@ except KeyError as e:
 except Exception as e:
   print(f'Other error: {e}')
 # save to new file
-df.to_excel('cleaned_grades_sample.xlsx', index = False)
+df.to_excel('cleaned_grades_sample.xlsx', index = False) # Save cleaned file
 print("Done! Check cleaned_grades_sample.xlsx")
 
